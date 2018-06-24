814b9fbb14a189ac5a60f0ba044c92ea962f20fd
jakeoeding <39394345+jakeoeding@users.noreply.github.com>
2018-06-24 11:27:14 -0500

    Update synonyms.py

--- synonyms.py
+++ synonyms.py
@@ -1,23 +1,23 @@
-from bs4 import BeautifulSoup
-from easygui import choicebox
-from keyboard import wait
-from pyperclip import paste
-from requests import get
+import bs4
+import easygui
+import keyboard
+import pyperclip
+import requests
 
 def get_html(word):
-    '''
+    """
     INPUT: a string of the word for which you want synonyms
     OUTPUT: html text of the synonym.com page pertaining to the input word
-    '''
-    wp = get(f'http://www.synonym.com/synonyms/{word}')
+    """
+    wp = requests.get(f'http://www.synonym.com/synonyms/{word}')
     return wp.text
 
 def parse_soup(soup):
-    '''
+    """
     INPUT: a BeautifulSoup object
     OUTPUT: a list of of synonyms parsed from the input if word is valid;
           otherwise, returns list containing only the error message
-    '''
+    """
     # check to see if there is an error message
     error_message = soup.select('div[id="notification"]')
     if len(error_message) == 1:
@@ -27,27 +27,27 @@ def parse_soup(soup):
     return message
 
 def display_synonyms(synonyms, word):
-    '''
+    """
     INPUT: the original word and the list of its synonyms
-    OUTPUT: a gui window that displays the synonyms
-    '''
-    choicebox(f'Synonyms of {word.upper()}:', choices=synonyms)
+    ACTION: opens a gui window that displays the synonyms
+    """
+    easygui.choicebox(f'Synonyms of {word.upper()}:', choices=synonyms)
 
 def lookup_synonyms(word):
-    '''
+    """
     INPUT: a string of the word for which you want synonyms
-    OUTPUT: a gui window that displays the synonyms
-    '''
+    ACTION: a gui window that displays the synonyms
+    """
     html = get_html(word)
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = bs4.BeautifulSoup(html, 'html.parser')
     synonyms = parse_soup(soup)
     display_synonyms(synonyms, word)
 
 if __name__ == '__main__':
     while True:
         # halt execution until this key combo is pressed
-        wait(combination='ctrl+g+h')
+        keyboard.wait(combination='ctrl+g+h')
         # assign the clipboard contents to 'word'
-        word = paste()
+        word = pyperclip.paste()
         # if clipboard wasn't blank, find synonym of contents
         if word != None: lookup_synonyms(word)
